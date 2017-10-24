052c5dbb5eaf555ff435d41ce74883a71d26e705
AnKain78 <33071818+AnKain78@users.noreply.github.com>
2017-10-24 23:52:30 +0200

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+       jshint: {
+           files: ['*.js']
+       } 
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+};
